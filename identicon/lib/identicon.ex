23ff4e9aed261b7726b0e99e8756ea4f26e9d477
usmailaabdoul <ismaelabdul77@gmail.com>
2023-01-09 07:41:37 +0100

    New project -> Identicon

---
+++ identicon/lib/identicon.ex
@@ -0,0 +1,26 @@
+defmodule Identicon do
+
+  def main(input) do
+    input
+    |> hash_input
+    |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |> Enum.chunk(3)
+    # |> mirror_row
+  end
+
+  def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | color: {r, g, b}}
+  end
+
+  def hash_input(input) do
+    hex = :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
+
+    %Identicon.Image{hex: hex}
+  end
+end
