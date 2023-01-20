fdf7fece27a31af9a8d52f24b5cfeffd4db7d897
usmailaabdoul <ismaelabdul77@gmail.com>
2023-01-20 07:29:43 +0100

    connecting to the database

--- discuss/lib/discuss_web/controllers/topic_controller.ex
+++ discuss/lib/discuss_web/controllers/topic_controller.ex
@@ -1,11 +1,22 @@
 defmodule DiscussWeb.TopicController do
   use DiscussWeb, :controller
+  # use Ecto.Repo,
 
   alias Discuss.Topic
+  alias Discuss.Repo
 
   def new(conn, _params) do
     changeset = Topic.changeset(%Topic{}, %{})
 
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} -> IO.inspect(changeset)
+    end
   end
 end
