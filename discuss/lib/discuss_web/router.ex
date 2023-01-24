f14131bb42b7c3513546192e38c0ceb9a518170a
usmailaabdoul <ismaelabdul77@gmail.com>
2023-01-24 07:06:56 +0100

    Adding edit, update and delete functionality

--- discuss/lib/discuss_web/router.ex
+++ discuss/lib/discuss_web/router.ex
@@ -20,6 +20,10 @@ defmodule DiscussWeb.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
+    delete "/topics/:id", TopicController, :delete
+    # resources "/", TopicController
   end
 
   # Other scopes may use custom stacks.
